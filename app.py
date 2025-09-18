6d81c4c2aca00f868f686163298b2b82f07d004c
Nikaalfa Shymon <nika2101nika@gmail.com>
2025-09-18 20:06:35 +0300

    Remove unused SQLAlchemy imports

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request, session, redirect, flash
+from flask import Flask, render_template
 import os
-from flask_sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask('test website')
+app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 @app.route('/')
