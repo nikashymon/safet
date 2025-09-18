52ede2746ab782a86232dea64d68ea046a567247
Nikaalfa Shymon <nika2101nika@gmail.com>
2025-09-18 14:05:41 +0300

    Updated app.py for Render deployment

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, session, redirect, flash
+import os
+from flask_sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask('test website')
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+def home():
+    return render_template('с.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
